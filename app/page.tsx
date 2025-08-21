4cc14d0fa209f29e1d5086a14d19c65f31a09618
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-21 11:24:31 +0530

    shad cn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,34 @@
 import React from "react";
 import Demo from "./demo";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const page = () => {
   return (
-    <div className="text-3xl text-primary-500 ">
-      <h1 className="h1-bold text-link-100 font-absans">Ai Flow</h1>
-      Ai Flow !
-      <Demo/>
-    </div>
+    <>
+      <div className="text-3xl text-primary-500 ">
+        <h1 className="h1-bold text-link-100 font-absans">Ai Flow</h1>
+        Ai Flow !
+        <Demo />
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 };
 
