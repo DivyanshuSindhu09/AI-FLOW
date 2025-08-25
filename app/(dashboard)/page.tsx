1e5c6e26d43190b30bda2b7d01ba24d77450ad16
Divyanshu Sindhu <divyanshu.sindhu@gmail.com>
2025-08-25 17:06:08 +0530

    basic structure of home component

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -18,14 +18,14 @@
 //   const session = await auth();
 //   console.log(session);
 
-  //   {
-  //   user: {
-  //     name: 'Divyanshu Sindhu',
-  //     email: 'divyanshu.sindhu@gmail.com',
-  //     image: 'https://avatars.githubusercontent.com/u/183302458?v=4'
-  //   },
-  //   expires: '2025-09-21T15:05:16.669Z'
-  // }
+//   {
+//   user: {
+//     name: 'Divyanshu Sindhu',
+//     email: 'divyanshu.sindhu@gmail.com',
+//     image: 'https://avatars.githubusercontent.com/u/183302458?v=4'
+//   },
+//   expires: '2025-09-21T15:05:16.669Z'
+// }
 
 //   {
 //   user: {
@@ -56,12 +56,34 @@
 
 // export default page;
 
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+import Link from "next/link";
+import React from "react";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <>
+    <section className="flex w-full flex-col-reverse justify-between gap-2 sm:flex-row sm:flex-center">
+      <h1 className="h1-bold text-dark100_light900 font-absans">All Questions</h1>
+      <Button asChild 
+      className="primary-gradient min-h-[30px] px-4 py-3 !text-light-900"
+      >
+        <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+      </Button>
+    </section>
+    <section className="mt-4">
+      Local Search
+    </section>
+    Home Filter
+    <div className="mt-4 flex w-full flex-col gap-6">
+      <p>Question card 1</p>
+      <p>Question card 1</p>
+      <p>Question card 1</p>
+      <p>Question card 1</p>
+    </div>
+    </>
+  );
+};
 
-export default page
+export default page;
